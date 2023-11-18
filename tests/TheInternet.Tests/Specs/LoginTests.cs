bc1356043207b7bd51b9daeeade2316d67662a83
Mitchell Ramos <mitchell.ramos@aurizon.com.au>
2023-11-18 12:02:36 +1000

    Refactor login test

--- tests/TheInternet.Tests/Specs/LoginTests.cs
+++ tests/TheInternet.Tests/Specs/LoginTests.cs
@@ -1,23 +1,26 @@
-using System;
 using FluentAssertions;
 using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.DevTools;
 using TheInternet.Tests.Pages;
 
 namespace TheInternet.Tests.Specs;
 
 public class LoginTests : BaseTest
 {    
+    LoginPage? loginPage; 
+
+    [SetUp]
+    public new void Setup()
+    {
+        driver!.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
+        loginPage = new(driver!);
+    }
+
     [Test]
     public void ReturnsSuccessful_When_Credentials_AreValid()
     {        
-        driver!.Navigate().GoToUrl("https://the-internet.herokuapp.com/login");
-
-        LoginPage loginPage = new(driver);
-        loginPage.Login("tomsmith", "SuperSecretPassword!");
-
+        loginPage!.Login("tomsmith", "SuperSecretPassword!");
         bool isLoggedIn = loginPage.IsLoggedIn();
+        
         isLoggedIn.Should().BeTrue();
     }
 
